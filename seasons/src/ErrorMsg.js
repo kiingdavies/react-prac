9dccbd92b43bf9641b7045c976a87586c6ac635c
kiingdavies <daviesadedeji007@gmail.com>
2019-11-27 23:31:23 +0100

    alot done

---
+++ seasons/src/ErrorMsg.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorMsg = () => {
+  return (
+    <div className="ui negative message">
+      <div className="header">
+        We're sorry we can't get your location at the moment, please try again!
+      </div>
+    </div>
+  );
+};
+
+export default ErrorMsg;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import ErrorMsg from './ErrorMsg';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-        return <div>Latitude: </div>
-    }
-};
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+  componentDidMount() {
+    console.log("component mounted");
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({ errorMessage: err.message })
+    )
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat ) {
+      return <ErrorMsg />;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
