20ded0b32dfe1f18e568c0fac10f9ccd2a7455e4
kiingdavies <daviesadedeji007@gmail.com>
2019-11-28 08:51:57 +0100

    Done

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
-import ErrorMsg from './ErrorMsg';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import ErrorMsg from "./ErrorMsg";
 
 class App extends React.Component {
   state = {
@@ -14,21 +14,31 @@ class App extends React.Component {
     console.log("component mounted");
 
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({lat: position.coords.latitude}),
+      position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
-    )
+    );
   }
 
-  render() {
-    if (this.state.errorMessage && !this.state.lat ) {
+  //This is a helper method decides what content to render
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
       return <ErrorMsg />;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Kindly accept location request" />;
+  }
+
+  // React says we must define render
+  render() {
+    return (
+      //The border red wont show cos it wasnt styled,
+      //its just an example of a case where we want all our results to show up in a specific styled container
+      <div className="border red">{this.renderContent()}</div>
+    );
   }
 }
 
