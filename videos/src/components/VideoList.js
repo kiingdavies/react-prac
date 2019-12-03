7439ca8b3da9bb9bd96bd2e156cdf6b30fd799df
kiingdavies <daviesadedeji007@gmail.com>
2019-12-03 05:43:41 +0100

    Errors fixed

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -4,7 +4,13 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   // use props.videos if u dont destructure like this ({ videoes })
   const renderedList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
