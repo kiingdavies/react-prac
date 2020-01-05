24f7af46e96da43a89aae759678def613c751bee
kiingdavies <daviesadedeji007@gmail.com>
2020-01-05 23:48:41 +0100

    translate done

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -21,7 +24,11 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        {this.state.language}
+        <ColorContext.Provider value="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
